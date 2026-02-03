0b4d927e3f99388b776e511554e62b54fc3fadf8
SmirnovaNadia <smirnova2402nv@gmail.com>
2026-02-03 13:28:11 +0400

    Добавила TestPet

---
+++ src/test/java/TestPet.java
@@ -0,0 +1,29 @@
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+
+@
+public class TestPet {
+    private static final String BASE_URL = "http://5.181.109.28:9090/api/v3";
+
+    @Test
+    public void testDeletedNonexistentPet() {
+        Response response = given()
+                .contentType(ContentType.JSON)
+                .header("Accept", "application/json")
+                .when()
+                .delete(BASE_URL+ "/pet/9999");
+
+        String responseBody = response.getBody().asString();
+        assertEquals(200, response.getStatusCode(),
+                "Код ответа не совпал с ожидаемым. Ответ: " + responseBody);
+
+        assertEquals("Pet deleted", responseBody,
+                "Текст ошибки не совпал с ожидаемым. Получен: " + responseBody);
+    }
+}
